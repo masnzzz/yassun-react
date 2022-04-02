0072b3a0afd71a03834d354adcb99a1abae78425
masnzzz <z3heng0326@gmail.com>
2022-04-02 14:05:04 +0900

    繰り返しによるリスト表示をする
    description、変更ボタンを削除した

--- src/List.js
+++ src/List.js
@@ -1,8 +1,23 @@
-export const List = ({title}) => {
+const LANGAGES = [
+    'JavaScript',
+    'C++',
+    'PHP',
+    'Go',
+    'Ruby'
+];
+
+export const List = () => {
     return (
         <div>
-            <h4>{ title }</h4>
-            <div>リストです</div>
+            {
+                LANGAGES.map((lang, index) => {
+                    return <div key={index}>{ lang }</div>
+                })
+            }
         </div>
     )
+    // LANGAGEの中を繰り返し表示できるようにする
+    // map関数、配列を変換して別の配列にする関数、ここでは言語の文字列のリストをjsxのhtmlのリストに変更する
+    // jsxで繰り返すときは、keyプロパティを設定する必要あり、（keyはユニークである必要）
+    // mapの二つ目の引数は配列の要素の0から始まるindex（number）がついてくる
 }
