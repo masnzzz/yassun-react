4c2d62c6eec2611ead7cba82079ea746b597f84c
masnzzz <z3heng0326@gmail.com>
2022-04-02 13:36:25 +0900

    クラスコンポーネントで書き換える

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import { useState } from 'react';
+import React from 'react';
 import { List } from "./List";
 
- function App() {
-   const [description, setDescription] = useState('クリック前の表示');
+class App extends React.Component {
+  // ステートを使うときは、
+  constructor(props) {
+    super(props);
+    // クラスコンポーネントでは this.state がステートになる
+    // 初期値は ’クリック前の表示’
+    this.state = { description: 'クリック前の表示' }
+  }
 
-   const changeDescription = () => {
-     setDescription('クリック後の表示です');
-   }
-  return (
-    <div>
-      { description }
-      <List title="取扱言語一覧" />
-      <button onClick={changeDescription}>ボタン</button>
-    </div>
-  );
+  // Descriptionを変更する関数
+  changeDescription() {
+    // setState はオブジェクトを受け取り
+    this.setState({
+      // 変更後の値を入れる
+      description: 'クリック後の表示です。'
+    })
+  }
+
+  render() {
+    // this.state でステートを取り出し、descriptionを呼び出す
+    const { description } = this.state;
+    return (
+      <div>
+        { description }
+        <List title="取扱言語一覧" />
+        <button onClick={() => this.changeDescription()}>
+          ボタン
+        </button>
+      </div>
+    )
+    // クラスのメソッドを呼び出すので、thisが引き継がれている必要あり。
+    // アロー関数 () => でthisを引き継ぐ
+  }
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,8 +1,14 @@
-export const List = ({title}) => {
-    return (
+import React from "react";
+export class List extends React.Component {
+    render() {
+        // this.props にプロパティが入っている
+      const { title } = this.props;
+      return (
         <div>
-            <h4>{ title }</h4>
-            <div>リストです</div>
+          <h4>{ title }</h4>
+          <div>リストです</div>
         </div>
-    )
-}
+      )
+    }
+  }
+  
