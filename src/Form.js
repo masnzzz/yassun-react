a73db7d57d9aa6e4a938a5884f58c3a64d79c729
masnzzz <z3heng0326@gmail.com>
2022-04-03 13:56:32 +0900

    フォームを作成
    入力された値がコンソールに表示されるようにした

--- src/Form.js
+++ src/Form.js
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+
 export const Form = () => {
-    return <div>フォームです。</div>
+    // text ステートを定義した
+    const [text, setText] = useState('');
+
+    const submitForm = (e) => {
+        // ページ遷移が起きないようにしている
+        e.preventDefault();
+        console.log(text);
+    }
+
+    return (
+        <div>
+            <h4>新しい言語の追加</h4>
+            <form onSubmit={submitForm}>
+                <div>
+                    <input
+                     type="text"
+                     value={text}
+                     onChange={(e) => setText(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <button>追加</button>
+                </div>
+            </form>
+        </div>
+    )
 }
