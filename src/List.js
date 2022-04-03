511a6b699a16939d1601d5bb9845fe12f95adf0e
masnzzz <z3heng0326@gmail.com>
2022-04-03 17:17:35 +0900

    [ライフサイクル]unmountingのタイミング

--- src/List.js
+++ src/List.js
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
+
 // 親コンポーネントApp.jsから受け取ったlangs配列（言語のリスト）を取得
 export const List = ({ langs }) => {
-    return (
-        <div>
-            {
-                langs.map((lang, index) => {
-                    return <div key={index}>{ lang }</div>
-                })
-            }
-        </div>
-    )
-    // LANGAGEの中を繰り返し表示できるようにする
-    // map関数、配列を変換して別の配列にする関数、ここでは言語の文字列のリストをjsxのhtmlのリストに変更する
-    // jsxで繰り返すときは、keyプロパティを設定する必要あり、（keyはユニークである必要）
-    // mapの二つ目の引数は配列の要素の0から始まるindex（number）がついてくる
+
+  useEffect(() => {
+    console.log('List.js:useEffect');
+
+    return () => {
+      console.log('List.js:useEffect:unmount');
+    }
+  })
+
+  return (
+      <div>
+          {
+              langs.map((lang, index) => {
+                  return <div key={index}>{ lang }</div>
+              })
+          }
+      </div>
+  )
+  // LANGAGEの中を繰り返し表示できるようにする
+  // map関数、配列を変換して別の配列にする関数、ここでは言語の文字列のリストをjsxのhtmlのリストに変更する
+  // jsxで繰り返すときは、keyプロパティを設定する必要あり、（keyはユニークである必要）
+  // mapの二つ目の引数は配列の要素の0から始まるindex（number）がついてくる
 }
