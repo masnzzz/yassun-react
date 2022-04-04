1f89cb53c89900beccca176167e819bc5efad24c
masnzzz <z3heng0326@gmail.com>
2022-04-05 00:22:36 +0900

    [Reactのスタイル] StyledComponentsでフォームのスタイルを作成
    ボタンコンポーネントを作成

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,31 @@
+import styled, { StyleSheetManager } from 'styled-components';
 import { useState } from 'react';
+import { Button } from './components/button';
+
+const Countainer = styled.div`
+    padding: 12px 64px;
+`
+
+const Label = styled.label`
+    display: flex;
+    color: #757575;
+    font-size: 14px;
+    font-weight: bold;
+`
+
+const Input = styled.input`
+    border-radius: 3px;
+    padding: 4px 8px;
+    border: 1px solid black;
+`
+
+const ButtonContainer = styled.div`
+    margin-top: 24px;
+`
+
+const FormButton = styled(Button)`
+    width: 120px;
+`
 
 // 親コンポーネントApp.jsからonAddLang関数を取得
 export const Form = ({ onAddLang }) => {
@@ -13,20 +40,21 @@ export const Form = ({ onAddLang }) => {
     }
 
     return (
-        <div>
+        <Countainer>
             <h4>新しい言語の追加</h4>
             <form onSubmit={submitForm}>
                 <div>
-                    <input
+                    <Label>言語</Label>
+                    <Input
                      type="text"
                      value={text}
                      onChange={(e) => setText(e.target.value)}
                     />
                 </div>
-                <div>
-                    <button>追加</button>
-                </div>
+                <ButtonContainer>
+                    <FormButton>追加</FormButton>
+                </ButtonContainer>
             </form>
-        </div>
+        </Countainer>
     )
 }
--- src/List.js
+++ src/List.js
@@ -1,14 +1,29 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  padding: 12px 64px;
+`
+
+const ListItem = styled.div`
+ padding: 8px 16px;
+
+ &:nth-child(n+2) {
+   border-top: 1px solid #D9DBDE;
+ }
+
+`
+
 // 親コンポーネントApp.jsから受け取ったlangs配列（言語のリスト）を取得
 export const List = ({ langs }) => {
 
   return (
-      <div>
+      <Container>
           {
               langs.map((lang, index) => {
-                  return <div key={index}>{ lang }</div>
+                  return <ListItem key={index}>{ lang }</ListItem>
               })
           }
-      </div>
+      </Container>
   )
   // LANGAGEの中を繰り返し表示できるようにする
   // map関数、配列を変換して別の配列にする関数、ここでは言語の文字列のリストをjsxのhtmlのリストに変更する
